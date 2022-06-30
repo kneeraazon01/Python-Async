2f9606e1ae3207ed4079cf59a5b647bfe527d6df
kneeraazon01 <kneeraazon@gmail.com>
2022-07-01 00:27:57 +0545

    most basic example

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ import asyncio
 
 async def main():
     print("main 1")
-    await main2()
+    task = asyncio.create_task(main2())
     print("world 1")
 
 
 async def main2():
     print("main 2")
-    await asyncio.sleep(5)
-    print("world 2")
+    await asyncio.sleep(1)
 
 
 asyncio.run(main())
