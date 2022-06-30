024a584420608a98eb608cb4b75ffb57aa7acfeb
kneeraazon01 <kneeraazon@gmail.com>
2022-07-01 00:17:38 +0545

    most basic example

---
+++ main.py
@@ -0,0 +1,18 @@
+print("fuck async")
+import asyncio
+
+
+async def main():
+    print("main 1")
+    await main2()
+    print("world 1")
+
+
+async def main2():
+    print("main 2")
+    await asyncio.sleep(5)
+    print("world 2")
+
+
+asyncio.run(main())
+print("end")
